a238738a11bd10f6bcdc0912ec1cb3cf28c355c7
narender <narender2031@gmail.com>
2018-02-05 19:29:22 +0530

    minior chnage in appliction UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+l = Location.create!(location_name: 'Chandigrah', total_meeting_room: 2)
+m = MeetingRoom.create!(name: "Meeting romm 1", location_id: 1);
+u = User.create!(name: "example", phone:"9999999999", email:"user@example.com", password:"manu2031", location_id:1)
