af06533b123b72bd2458be7ca9e7c84be2f1d180
narender <narender2031@gmail.com>
2018-02-06 16:48:59 +0530

    aws

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '139.59.40.80', roles: [:web, :app, :db], primary: true
+
+server '13.126.173.85', roles: [:web, :app, :db], primary: true
