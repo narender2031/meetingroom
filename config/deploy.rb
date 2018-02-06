6428ce4fa2e8d39021953de45ea02ad4c01e790d
narender <narender2031@gmail.com>
2018-02-06 16:51:19 +0530

    aws 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
-
+set :ssh_options,{ forward_agent: true, user: 'deploy', keys: %w(~/.ssh/id_rsa) }
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
